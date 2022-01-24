e6a5953e4c88517c412bfba03cf0b3a78f098b8c
carlos4565mana <carloscal61@gmail.com>
2022-01-24 00:45:35 -0300

    add readme

--- dio-banco/src/ContaPoupanca.java
+++ dio-banco/src/ContaPoupanca.java
@@ -1,10 +1,14 @@
 
 public class ContaPoupanca extends Conta {
 	
+	public ContaPoupanca(Cliente cliente) {
+		super(cliente);
+	}
+
 	@Override
 	public void imprimirExtrato() {
-		System.out.println("****Extrato de Conta Poupança*******");
-		imprimirInfoCommuns();
+		System.out.println("=== Extrato Conta Corrente ===");
+		super.imprimirInfosComuns();
 	}
 
 }
